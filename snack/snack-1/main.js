190995a01ab990bdffcd6cc4ea6a016eabdbb9d1
GianniC88 <gianni.colucci.88@gmail.com>
2025-05-30 11:16:25 +0200

    Aggiungi file HTML e JavaScript per il primo snack: struttura base e logica per confrontare due numeri

---
+++ snack/snack-1/main.js
@@ -0,0 +1,12 @@
+//utente inserisce due numeri in successione,con due prompt. Il software stampa il maggiore
+console.log(ciao)
+
+const numOne : prompt("inserisci il primo numero");
+const numTwo : prompt("inserisci il secondo numero");
+
+if (numOne > numTwo) {
+	console.log(numOne);
+  } else {
+	num2 > num1;
+	console.log(numTwo);
+  }
