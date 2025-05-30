73422247a62003cf66f38b51d26a5a4341d5fe83
GianniC88 <gianni.colucci.88@gmail.com>
2025-05-30 11:22:18 +0200

    Correggi errori di sintassi e miglioramenti nella logica di confronto tra due numeri

--- snack/snack-1/main.js
+++ snack/snack-1/main.js
@@ -1,12 +1,9 @@
-//utente inserisce due numeri in successione,con due prompt. Il software stampa il maggiore
-console.log(ciao)
-
 const numOne : prompt("inserisci il primo numero");
 const numTwo : prompt("inserisci il secondo numero");
 
 if (numOne > numTwo) {
 	console.log(numOne);
   } else {
-	num2 > num1;
+	numTwo > numOne;
 	console.log(numTwo);
   }
